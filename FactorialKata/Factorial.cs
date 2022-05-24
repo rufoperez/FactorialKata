a07c3fa3eca081b044b23b9857d6a94d97f2a832
Rufino Perez <ruperez@aidacanarias.com>
2022-05-24 08:06:21 +0100

    red Zero_factorial_should_return_1

---
+++ FactorialKata/Factorial.cs
@@ -0,0 +1,9 @@
+﻿namespace FactorialKata;
+
+public class Factorial
+{
+    public static int GetFactorial(int i)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ FactorialKataTest/FactorialShould.cs
@@ -0,0 +1,17 @@
+﻿using FactorialKata;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace FactorialKataTest
+{
+    public class FactorialShould
+    {
+        [Test]
+        public void Zero_factorial_should_return_1()
+        {
+            var result = Factorial.GetFactorial(0);
+            
+            result.Should().Be(1);
+        }
+    }
+}
