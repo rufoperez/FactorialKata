cc1a6779b1f027a82788b0430edafcee5e6ce5cd
Rufino Perez <ruperez@aidacanarias.com>
2022-05-24 08:07:46 +0100

    green one_factorial_should_return_one

--- FactorialKataTest/FactorialShould.cs
+++ FactorialKataTest/FactorialShould.cs
@@ -7,10 +7,18 @@ namespace FactorialKataTest
     public class FactorialShould
     {
         [Test]
-        public void Zero_factorial_should_return_1()
+        public void zero_factorial_should_return_one()
         {
             var result = Factorial.GetFactorial(0);
             
+            result.Should().Be(1);
+        }
+
+        [Test]
+        public void one_factorial_should_return_one()
+        {
+            var result = Factorial.GetFactorial(1);
+
             result.Should().Be(1);
         }
     }
