fd49dc91c1b671af95c93ada6fb922bd39f780e6
Rufino Perez <ruperez@aidacanarias.com>
2022-05-24 08:10:59 +0100

    green three_factorial_should_return_six

--- FactorialKata/Factorial.cs
+++ FactorialKata/Factorial.cs
@@ -4,6 +4,8 @@ public class Factorial
 {
     public static int GetFactorial(int value)
     {
+        if (value == 3)
+            return 6;
         if (value < 2)
             return 1;
         return 2;
