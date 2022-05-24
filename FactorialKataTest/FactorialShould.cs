48492bbcb4ee7413ae7cc92dbf7a6b28e2b8603b
Rufino Perez <ruperez@aidacanarias.com>
2022-05-24 08:15:53 +0100

    more green test

--- FactorialKataTest/FactorialShould.cs
+++ FactorialKataTest/FactorialShould.cs
@@ -11,6 +11,7 @@ namespace FactorialKataTest
         [TestCase(1, 1)]
         [TestCase(2, 2)]
         [TestCase(3, 6)]
+        [TestCase(8, 40320)]
         public void zero_factorial_should_return_one(int value, int expectedResult)
         {
             var result = Factorial.GetFactorial(value);
